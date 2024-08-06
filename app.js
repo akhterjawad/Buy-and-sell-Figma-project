69efcac3dc00e4961d4916dde755676a2e505421
akhterjawad <akhterjawad566@gmail.com>
2024-08-06 13:55:23 -0700

    75% complete

--- app.js
+++ app.js
@@ -33,28 +33,28 @@ form.addEventListener('submit', event => {
         });
 });
 
-googleBtn.addEventListener(`click`, () => {
-    signInWithPopup(auth, provider)
-        .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            // IdP data available using getAdditionalUserInfo(result)
-            // ...
-            console.log(user);
-            window.location = `home.html`;
-        }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-            console.log(credential);
+// googleBtn.addEventListener(`click`, () => {
+//     signInWithPopup(auth, provider)
+//         .then((result) => {
+//             // This gives you a Google Access Token. You can use it to access the Google API.
+//             const credential = GoogleAuthProvider.credentialFromResult(result);
+//             const token = credential.accessToken;
+//             // The signed-in user info.
+//             const user = result.user;
+//             // IdP data available using getAdditionalUserInfo(result)
+//             // ...
+//             console.log(user);
+//             window.location = `home.html`;
+//         }).catch((error) => {
+//             // Handle Errors here.
+//             const errorCode = error.code;
+//             const errorMessage = error.message;
+//             // The email of the user's account used.
+//             const email = error.customData.email;
+//             // The AuthCredential type that was used.
+//             const credential = GoogleAuthProvider.credentialFromError(error);
+//             // ...
+//             console.log(credential);
 
-        });
-})
+//         });
+// });
