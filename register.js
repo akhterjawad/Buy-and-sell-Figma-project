3840e084b3db1be178674dce941f5c77c678984d
akhterjawad <akhterjawad566@gmail.com>
2024-08-06 13:42:57 -0700

    75% complete

--- register.js
+++ register.js
@@ -1,25 +1,32 @@
-// import { createUserWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-// import { auth } from "./config.js";
-// // import { auth } from "./config.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { auth } from "./config.js";
 
 
 let form = document.querySelector('#form');
-// let email = document.querySelector('#email');
-// let password = document.querySelector('#password');
-form.addEventListener('submit',event=>{
+let email = document.querySelector('#email');
+let password = document.querySelector('#password');
+form.addEventListener('submit', event => {
     event.preventDefault();
-    console.log(`h`);
-    
-//     createUserWithEmailAndPassword(auth, email.value, password.value)
-//       .then((userCredential) => {
-//         // Signed up 
-//         const user = userCredential.user;
-//         console.log(user);
-//         window.location=`index.html`;
-//       })
-//       .catch((error) => {
-//         const errorCode = error.code;
-//         const errorMessage = error.message;
-//         console.log(errorMessage);
-//       });
-})
+
+
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            alert('You are registered');
+            console.log(user);
+            window.location = `index.html`;
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage);
+            if (email.value == '' && password.value == '') {
+                alert('Please fill in the input fields');
+            } else {
+                alert(errorMessage);
+            }
+            email.value = '';
+            password.value = '';
+        });
+});
