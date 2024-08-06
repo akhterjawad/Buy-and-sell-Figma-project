292952b7fa8fb603aee459a6f90f8d39a879227c
akhterjawad <akhterjawad566@gmail.com>
2024-08-06 01:55:28 -0700

    40% complete

--- app.js
+++ app.js
@@ -1,55 +1,55 @@
-import { signInWithEmailAndPassword,
-    GoogleAuthProvider,
-    signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-import { auth } from "./config.js";
-const provider = new GoogleAuthProvider();
+// import { signInWithEmailAndPassword,
+//     GoogleAuthProvider,
+//     signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+// import { auth } from "./config.js";
+// const provider = new GoogleAuthProvider();
 
 
-let form = document.querySelector('#form');
-let email = document.querySelector('#email');
-let password = document.querySelector('#password');
-let googleBtn = document.querySelector('.googlelogin');
+// let form = document.querySelector('#form');
+// let email = document.querySelector('#email');
+// let password = document.querySelector('#password');
+// let googleBtn = document.querySelector('.googlelogin');
 
 
-form.addEventListener('submit',event=>{
-    event.preventDefault();     
-signInWithEmailAndPassword(auth, email.value, password.value)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user);
-    window.location=`home.html`;
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage);
+// form.addEventListener('submit',event=>{
+//     event.preventDefault();     
+// signInWithEmailAndPassword(auth, email.value, password.value)
+//   .then((userCredential) => {
+//     // Signed in 
+//     const user = userCredential.user;
+//     console.log(user);
+//     window.location=`home.html`;
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     console.log(errorMessage);
     
-  });
-});
+//   });
+// });
 
-googleBtn.addEventListener(`click`,()=>{
-    signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
-      console.log(user);
-      window.location=`home.html`;
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-      console.log(credential);
+// googleBtn.addEventListener(`click`,()=>{
+//     signInWithPopup(auth, provider)
+//     .then((result) => {
+//       // This gives you a Google Access Token. You can use it to access the Google API.
+//       const credential = GoogleAuthProvider.credentialFromResult(result);
+//       const token = credential.accessToken;
+//       // The signed-in user info.
+//       const user = result.user;
+//       // IdP data available using getAdditionalUserInfo(result)
+//       // ...
+//       console.log(user);
+//       window.location=`home.html`;
+//     }).catch((error) => {
+//       // Handle Errors here.
+//       const errorCode = error.code;
+//       const errorMessage = error.message;
+//       // The email of the user's account used.
+//       const email = error.customData.email;
+//       // The AuthCredential type that was used.
+//       const credential = GoogleAuthProvider.credentialFromError(error);
+//       // ...
+//       console.log(credential);
       
-    });    
-})
+//     });    
+// })
--- register.js
+++ register.js
@@ -1,23 +1,23 @@
-import { createUserWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-import { auth } from "./config.js";
+// import { createUserWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 // import { auth } from "./config.js";
+// // import { auth } from "./config.js";
 
 
-let form = document.querySelector('#form');
-let email = document.querySelector('#email');
-let password = document.querySelector('#password');
-form.addEventListener('submit',event=>{
-    event.preventDefault();
-    createUserWithEmailAndPassword(auth, email.value, password.value)
-      .then((userCredential) => {
-        // Signed up 
-        const user = userCredential.user;
-        console.log(user);
-        window.location=`index.html`;
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorMessage);
-      });
-})
+// let form = document.querySelector('#form');
+// let email = document.querySelector('#email');
+// let password = document.querySelector('#password');
+// form.addEventListener('submit',event=>{
+//     event.preventDefault();
+//     createUserWithEmailAndPassword(auth, email.value, password.value)
+//       .then((userCredential) => {
+//         // Signed up 
+//         const user = userCredential.user;
+//         console.log(user);
+//         window.location=`index.html`;
+//       })
+//       .catch((error) => {
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+//         console.log(errorMessage);
+//       });
+// })
